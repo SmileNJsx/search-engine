bca4c85e532bf7e5f79a4b46aff98237bebc81a2
Jimmy <731314806@qq.com>
2016-03-08 22:14:49 +0800

    ADD src/

---
+++ src/search/engine/spider/HtmlParser.java
@@ -0,0 +1,58 @@
+package search.engine.spider;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class HtmlParser {
+	
+	Document doc;
+	
+	HtmlParser(String url) throws IOException{//必须进行构造函数
+		doc=Jsoup.connect(url).get();
+	}
+	
+	public HashMap<String,String> getParserMedia(){//解析媒体链接
+		HashMap<String,String> mapMedia=new HashMap<>();
+		Elements media=doc.select("[src]");
+		for(Element src:media){
+			mapMedia.put(src.tagName(),src.attr("abs:src"));
+		}
+		return mapMedia;
+	}
+	
+	public HashMap<String,String> getParserImports(){//解析插件链接
+		HashMap<String,String> mapImports=new HashMap<>();
+		Elements imports=doc.select("link[href]");
+		for(Element link:imports){
+			mapImports.put(link.attr("rel"),link.attr("abs:href"));
+		}
+		return mapImports;
+	}
+	
+	public HashMap<String,String> getParserKeylinks(){//解析关键字链接
+		HashMap<String,String> mapLinks=new HashMap<>();
+		Elements links=doc.select("a[href]");
+		for(Element link:links){
+			mapLinks.put(trim(link.text(),35),link.attr("abs:href"));
+		}
+		return mapLinks;
+	}
+	
+	private static String trim(String s,int width){
+		
+		if(s.length()>width){
+			return s.substring(0,width-1)+".";
+		}else{
+			return s;
+		}
+	}
+	
+	//TODO DELETE
+	public static void main(String args[]) throws IOException{
+	}
+}
---
+++ src/search/engine/spider/RetrivePage.java
@@ -0,0 +1,52 @@
+package search.engine.spider;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class RetrivePage {
+	public String getContent(String url) throws Exception{
+		CloseableHttpClient httpClient=HttpClients.createDefault();
+		try{
+			HttpGet httpGet=new HttpGet(url);
+			System.out.println("Executing Request  "+httpGet.getURI()+"...");
+			
+			ResponseHandler<String> responseHandler=new ResponseHandler<String>(){
+
+				@Override
+				public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+					// TODO Auto-generated method stub
+					
+					int status=response.getStatusLine().getStatusCode();
+					if(status>=200&&status<300){
+						HttpEntity entity=response.getEntity();
+						return entity != null ? EntityUtils.toString(entity):null;
+					}
+					else{
+						throw new ClientProtocolException("Unexpected Response Status:"+status);
+					}
+				}
+			};
+			
+			String responseBody=httpClient.execute(httpGet, responseHandler);
+			return responseBody;
+
+		}finally{
+			httpClient.close();
+		}
+	}
+	
+	public static void main(String args[]) throws Exception{
+		//TODO	DELETE
+		RetrivePage page=new RetrivePage();
+		page.getContent("http://www.baidu.com");
+	}
+	
+}
