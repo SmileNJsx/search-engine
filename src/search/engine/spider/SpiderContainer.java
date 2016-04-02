b6e8fa5946e69c2f00e8a15a7dbfd2ae99387563
Jimmy <731314806@qq.com>
2016-04-02 21:48:49 +0800

    MOD THREAD

--- src/search/engine/spider/SpiderContainer.java
+++ src/search/engine/spider/SpiderContainer.java
@@ -2,5 +2,15 @@ package search.engine.spider;
 
 public class SpiderContainer
 {
+    public static final int MAX_SPIDER_COUNT = 3;
     
+    public void retrivePage()
+    {
+    	for(int i=0;i<MAX_SPIDER_COUNT;i++)
+    	{
+    		SpiderThread spiderThread = new SpiderThread();
+    		Thread thread = new Thread(spiderThread);
+    		thread.start();
+    	}
+    }
 }
