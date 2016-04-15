250995ef76ecba25b75dad614d68e05c6597eb79
Jimmy <731314806@qq.com>
2016-04-15 14:02:53 +0800

    IMPROVE spider

--- src/search/engine/spider/SpiderContainer.java
+++ src/search/engine/spider/SpiderContainer.java
@@ -17,7 +17,7 @@ public class SpiderContainer
     	for(int i=0;i<MAX_SPIDER_COUNT;i++)
     	{
     		
-    		SpiderThread spiderThread = new SpiderThread(conn);
+    		SpiderThread spiderThread = new SpiderThread();
     		Thread thread = new Thread(spiderThread);
     		thread.start();
     	}
